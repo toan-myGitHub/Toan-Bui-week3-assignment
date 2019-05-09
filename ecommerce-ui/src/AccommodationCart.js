2a14233f93b7b71a0f0b0b31f72e2c98b37d4c70
toan-myGitHub <47186111+toan-myGitHub@users.noreply.github.com>
2019-05-09 01:12:22 -0700

    Create AccommodationCart.js

---
+++ ecommerce-ui/src/AccommodationCart.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class AccommodationCart extends React.Component {
+    render() {
+        const {title, 
+            image, 
+            houseType, 
+            city,
+            country,
+            cost,
+            description,
+            name,
+            isSuperhost,
+            stars,
+            reviews,
+            onClick, idx, buttonText} = this.props;
+        return (           
+          <div class='Accommodation-card'> 
+          <img src={image} alt='' class='Accommodation-img'></img>
+          <div class="Accommodation-container">
+          <h4>{title}</h4>
+          <b>Type</b>: {houseType}
+
+          <br></br><b>Location</b>: {city}, {country}
+                <p><b>Cost</b>: ${cost}
+                <br></br>{description}
+                </p><p><b>Host</b>: {name}
+                <br></br><b>Is host a Superhost</b>: {isSuperhost}
+                <br></br><b>Rating</b>: {stars} Star
+                <br></br><b>Reviews</b>: {reviews}
+                </p>
+
+<p><button onClick={() => onClick(idx)}>{buttonText}</button>
+</p>
+
+          
+          </div>
+</div>     
+        )
+    }
+
+  
+
+}
